4225794b674fee1f13431a93b95952f7e7e34797
Bharanitharan-M <m.bharanitharan2004@gmail.com>
2023-12-07 13:36:15 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Name: Bharanitharan M</h2>
+         <h2>Department : Information Technology</h2>
+        <h2>Year: 3rd year</h2>
+        <h2>Register number: 73772121111</h2>
+        <h2>Area of interest: Python and Web Development</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
